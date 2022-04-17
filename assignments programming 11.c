0dca12ff60df4c96ab321d108a2c96a8e313d39a
mutaiol <102149654+mutaiol@users.noreply.github.com>
2022-04-17 12:59:16 +0300

    Add files via upload

---
+++ assignments programming 11.c
@@ -0,0 +1,14 @@
+/*  a c program to show if a number is positive or negative */
+#include<stdio.h>
+int main ()
+{
+int num;
+printf("enter the number:");
+scanf("%d",&num);
+if(num>=0)
+printf("% d is positive num \n",num);
+else
+printf("%d is negative num \n",num);
+return 0;
+}
+
---
+++ programming 13 assignment.c
@@ -0,0 +1,20 @@
+//a program to give a discount of 10% if amount of items purchased exceed sh 1000
+#include<stdio.h>
+int main()
+{
+double k=0.1;
+double amount;
+double discount;
+printf("");("enter the amount:");
+scanf("");("% if",& amount);
+discount=amount*0.1;
+if(amount>1000)
+{
+printf("");("discount is % if",discount);
+}
+else
+{
+printf("");("sorry no discount is offered");
+}
+return 0;
+}
