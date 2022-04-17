a72bd7cfcd3e69b28ed19dfc382d03f27d8b4aef
mutaiol <102149654+mutaiol@users.noreply.github.com>
2022-04-17 12:47:07 +0300

    Add files via upload

---
+++ program to find sum of first 10 numbers.c
@@ -0,0 +1,14 @@
+//program to find sum of first 10 numbers
+#include<stdio.h>
+int main()
+{
+int i ,sum=0;
+printf("first 10 natural no's is:\n");
+for(i=1;i<=10;i++)
+{
+sum=sum+i;
+printf("%d",i);
+}
+printf("\n the sum is:%d\n",sum);
+return 0;
+}
